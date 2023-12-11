1e2d4b4960237646c145d58a6f7965d1bb0e046d
Mila Lavrova <mila.fourteen@gmail.com>
2023-12-11 11:50:59 +0300

    upload image method added

---
+++ CinemaCat.Api/Models/Image.cs
@@ -0,0 +1,9 @@
+﻿namespace CinemaCat.Api.Models
+{
+    public class Image
+    {
+        public string FullImageUrl { get; set; }
+
+        public string CompressedImageUrl { get; set; }
+    }
+}

27d10ae781cca6805eeec214e5fa6407aad27935
Mila Lavrova <mila.fourteen@gmail.com>
2024-01-05 00:43:39 +0300

    authorize attribute added

---
+++ CinemaCat.Application.Tests/Users/LoginUserHandlerTests.cs
@@ -0,0 +1,73 @@
+﻿using CinemaCat.Application.Configuration;
+using CinemaCat.Application.Handlers.Users.LoginUser;
+using CinemaCat.Application.Interfaces;
+using CinemaCat.Domain.Exceptions;
+using CinemaCat.Domain.Identity;
+using CinemaCat.Domain.Models;
+using Microsoft.Extensions.Options;
+using System.Linq.Expressions;
+
+namespace CinemaCat.Application.Tests.Users;
+public class LoginUserHandlerTests
+{
+    private readonly Mock<IDataBaseProvider<ApplicationUser>> _userProviderMock = new();
+    private readonly Mock<IOptions<JwtConfiguration>> _jwtConfiguration = new();
+    private readonly IFixture _fixture = new Fixture();
+
+    [Fact]
+    public async Task Handle_ThrowsException_ErrorEmail()
+    {
+        // arrange
+        var request = _fixture.Create<LoginUserRequest>();
+        _userProviderMock.Setup(m => m.GetAsync(It.IsAny<Expression<Func<ApplicationUser, bool>>>()))
+            .ReturnsAsync(new List<ApplicationUser>());
+        var _handler = new LoginUserHandler(_userProviderMock.Object, _jwtConfiguration.Object);
+
+        // act
+        var response = await _handler.Handle(request, CancellationToken.None);
+
+        // assert
+        response.IsSuccess.Should().BeFalse();
+        response.Error.Should().Be("User with that email does not exists");
+        response.Exception.Should().BeOfType<AuthorizationException>();
+    }
+
+    [Fact]
+    public async Task Handle_ThrowsException_ErrorPassword()
+    {
+        // arrange
+        var request = _fixture.Create<LoginUserRequest>();
+        var man = _fixture.Create<ApplicationUser>();
+        man.Email = request.Email;
+        _userProviderMock.Setup(m => m.GetAsync(It.IsAny<Expression<Func<ApplicationUser, bool>>>()))
+            .ReturnsAsync(new List<ApplicationUser>() { man });
+        var _handler = new LoginUserHandler(_userProviderMock.Object, _jwtConfiguration.Object);
+
+        // act
+        var response = await _handler.Handle(request, CancellationToken.None);
+
+        // assert
+        response.IsSuccess.Should().BeFalse();
+        response.Error.Should().Be("The password is wrong");
+        response.Exception.Should().BeOfType<AuthorizationException>();
+    }
+
+    [Fact]
+    public async Task Handle_Success_ReturnsResult()
+    {
+        // arrange
+        var request = _fixture.Create<LoginUserRequest>();
+        var man = _fixture.Create<ApplicationUser>();
+        man.Email = request.Email;
+        man.PasswordHash = IdentityUtils.GetPasswordHash(request.Password + man.Salt);
+        _userProviderMock.Setup(m => m.GetAsync(It.IsAny<Expression<Func<ApplicationUser, bool>>>()))
+            .ReturnsAsync(new List<ApplicationUser>() { man });
+        var _handler = new LoginUserHandler(_userProviderMock.Object, _jwtConfiguration.Object);
+
+        // act
+        var response = await _handler.Handle(request, CancellationToken.None);
+
+        // assert
+        response.IsSuccess.Should().BeTrue();
+    }
+}
---
+++ CinemaCat.Application.Tests/Users/RegisterUserHandlerTests.cs
@@ -0,0 +1,50 @@
+﻿using CinemaCat.Application.Handlers.Users.RegisterUser;
+using CinemaCat.Application.Interfaces;
+using CinemaCat.Domain.Exceptions;
+using CinemaCat.Domain.Models;
+using System.Linq.Expressions;
+
+namespace CinemaCat.Application.Tests.Users;
+public class RegisterUserHandlerTests
+{
+    private readonly Mock<IDataBaseProvider<ApplicationUser>> _userProviderMock = new();
+    private readonly IFixture _fixture = new Fixture();
+
+    [Fact]
+    public async Task Handle_ThrowsException_Error()
+    {
+        // arrange
+        var request = _fixture.Create<RegisterUserRequest>();
+        var man = _fixture.Create<ApplicationUser>();
+        man.Email = request.Email;
+        _userProviderMock.Setup(m => m.GetAsync(It.IsAny<Expression<Func<ApplicationUser, bool>>>()))
+            .ReturnsAsync(new List<ApplicationUser> { man });
+        var _handler = new RegisterUserHandler(_userProviderMock.Object);
+
+        // act
+        var response = await _handler.Handle(request, CancellationToken.None);
+
+        // assert
+        response.IsSuccess.Should().BeFalse();
+        response.Error.Should().Be("User with that email already exists");
+        response.Exception.Should().BeOfType<AuthorizationException>();
+    }
+
+    [Fact]
+    public async Task Handle_Success_ReturnsResult()
+    {
+        // arrange
+        var request = _fixture.Create<RegisterUserRequest>();
+        _userProviderMock.Setup(m => m.GetAsync(It.IsAny<Expression<Func<ApplicationUser, bool>>>()))
+            .ReturnsAsync(new List<ApplicationUser>());
+        _userProviderMock.Setup(m => m.CreateAsync(It.IsAny<ApplicationUser>()))
+           .ReturnsAsync((ApplicationUser x) => x);
+        var _handler = new RegisterUserHandler(_userProviderMock.Object);
+
+        // act
+        var response = await _handler.Handle(request, CancellationToken.None);
+
+        // assert
+        response.IsSuccess.Should().BeTrue();
+    }
+}

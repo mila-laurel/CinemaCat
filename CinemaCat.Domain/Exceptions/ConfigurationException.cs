2ba4b39e63f0d376b2065c0e3ac9906284979752
Mila Lavrova <mila.fourteen@gmail.com>
2024-01-11 18:29:34 +0300

    Configuration cusom exception is added

---
+++ CinemaCat.Domain/Exceptions/ConfigurationException.cs
@@ -0,0 +1,9 @@
+﻿namespace CinemaCat.Domain.Exceptions;
+
+[Serializable]
+public class ConfigurationException : CinemaCatException
+{
+    public ConfigurationException() { }
+    public ConfigurationException(string message) : base(message) { }
+    public ConfigurationException(string message, Exception inner) : base(message, inner) { }
+}
